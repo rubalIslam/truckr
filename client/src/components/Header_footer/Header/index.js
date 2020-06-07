be3bc2d6fbd337b79aeb2b040734f21bb140a54a
unknown <bedarullaskar@gmail.com>
2020-06-07 23:16:25 +0530

    changed routes server

--- client/src/components/Header_footer/Header/index.js
+++ client/src/components/Header_footer/Header/index.js
@@ -122,7 +122,7 @@ class Header extends Component {
                 <div className="container">
                     <div className="left">
                         <div className="logo">
-                            TH
+                            TH ট্রাক আর
                         </div>
                     </div>
                     <div className="right">
